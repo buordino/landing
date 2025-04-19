3c880204711dfde76d79756bb36cca0d1326af37
unknown <mh.naderi1386@gmail.com>
2025-04-19 14:46:17 +0330

    add a header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from "react";
 import "./globals.css";
 import { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
+import Header from "./_components/Header";
+import Footer from "./_components/Footer";
 const vazir = Vazirmatn({
   display: "swap",
   subsets: ["latin"],
@@ -20,7 +22,9 @@ export default function RootLayout({ children }: RootLayoutPropsType) {
   return (
     <html lang="fa" dir="rtl">
       <body className={`${vazir.className} overflow-x-hidden antialiased `}>
-        {children}
+        <Header />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
