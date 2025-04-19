64b078e918aad715a91bf0813de263b866ba71a1
unknown <mh.naderi1386@gmail.com>
2025-04-19 15:50:08 +0330

    change ui

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -1,5 +1,5 @@
+import Image from "next/image";
 import Link from "next/link";
-
 const navigationItems = [
   { text: "صفحه اصلی", href: "/" },
   { text: "توسعه دهندگان", href: "/" },
@@ -13,10 +13,14 @@ const Navigation = () => {
         <Link
           role="tab"
           className=" tab !text-custome-black hover:!text-custome-orange-2 "
-         
           href={nav.href}
           key={index}
         >
+          {index === 0 && (
+            <div  className="relative size-3.5 ml-1">
+              <Image className="object-center" fill src={"/SVGs/Star 2.svg"} alt="Star-2" />
+            </div>
+          )}
           {nav.text}
         </Link>
       ))}
