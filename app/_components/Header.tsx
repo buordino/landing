321d9485a1e5b9f09089c2499bd0ef54c407c806
unknown <mh.naderi1386@gmail.com>
2025-04-19 16:06:21 +0330

    add a mobile menu

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,12 +1,16 @@
 import Logo from "./Logo";
+import MobileMenu from "./MobileMenu";
 import Navigation from "./Navigation";
 
 const Header = () => {
   return (
     <header>
-      <div className="flex items-center px-12 py-4">
+      <div className="flex justify-between sm:justify-normal items-center px-12 py-4">
         <div className="hidden sm:grow sm:flex sm:items-center sm:justify-center">
           <Navigation />
+        </div>
+        <div className="sm:hidden">
+          <MobileMenu />
         </div>
         <div>
           <Logo />
---
+++ app/_components/MobileMenu.tsx
@@ -0,0 +1,35 @@
+import { IoMdMenu } from "react-icons/io";
+
+const MobileMenu = () => {
+  return (
+    <div className="drawer">
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        <label
+          htmlFor="my-drawer"
+          className="btn bg-custome-orange-2 drawer-button"
+        >
+            <IoMdMenu size={25} color="white" />
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default MobileMenu;
