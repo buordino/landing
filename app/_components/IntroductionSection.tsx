d9c61269a6bc6540727f789f7a6b2619f7c3e018
unknown <mh.naderi1386@gmail.com>
2025-04-20 19:11:34 +0330

    add a blur absolute element

--- app/_components/IntroductionSection.tsx
+++ app/_components/IntroductionSection.tsx
@@ -4,8 +4,8 @@ import PhoneImagesWrraper from "./PhoneImagesWrraper";
 const IntroductionSection = () => {
   return (
     <section id="main" className="relative">
-      {/* <div className="absolute top-[-200px] left-1/2 -translate-x-1/2 w-[700px] h-[700px] rounded-full bg-gradient-to-br from-orange-300 via-rose-300 to-red-400 opacity-40 blur-2xl z-0"></div> */}
-      <div className="py-22 px-42 relative bg-gradient-to-t from-circle-color to-white">
+      <div className="absolute w-[100%] bg-gradient-to-t from-custome-orange-1 to-circle-color  blur-[65px]  top-[25%] left-0 right-0 mx-auto bottom-0 rounded-t-[100%]"></div>
+      <div className="py-22 px-42 relative ">
         <div className="flex items-center justify-between gap-20">
           <MainContent />
           <PhoneImagesWrraper />
