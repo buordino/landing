0dfe19f7b2a7b3037c92a6f7340b9a1f79d1af09
unknown <mh.naderi1386@gmail.com>
2025-04-20 23:08:19 +0330

    make photos responsive

--- app/_components/IntroductionSection.tsx
+++ app/_components/IntroductionSection.tsx
@@ -4,7 +4,7 @@ import PhoneImagesWrraper from "./PhoneImagesWrraper";
 const IntroductionSection = () => {
   return (
     <section id="main" className="relative">
-      <div className="absolute w-[100%] bg-gradient-to-t from-custome-orange-1 to-circle-color  blur-[125px]  top-[25%] left-0 right-0 mx-auto bottom-0 rounded-t-[100%]"></div>
+      <div className="absolute w-[100%] bg-gradient-to-t from-custome-orange-1 to-circle-color blur-[125px] top-[25%] left-0 right-0 mx-auto bottom-0 rounded-t-[100%]"></div>
       <div className="py-22 px-24 md:px-42 relative ">
         <div className="flex justify-center flex-col 2lg:flex-row items-center 2lg:justify-between gap-10 sm:gap-20">
           <MainContent />
--- app/_components/PhoneImagesWrraper.tsx
+++ app/_components/PhoneImagesWrraper.tsx
@@ -4,7 +4,7 @@ const PhoneImagesWrraper = () => {
   return (
     <div className="basis-full 2lg:basis-[50%]">
       <div className="flex justify-end relative">
-        <div className="absolute left-24 sm:left-30 top-4 w-[300px] h-[340px] sm:w-[400px] sm:h-[440px] z-[2]">
+        <div className="absolute left-20 sm:left-30 top-4 w-[190px] h-[250px] sm:w-[380px] sm:h-[440px] z-[2]">
           <Image
             fill
             className="object-center"
@@ -12,7 +12,7 @@ const PhoneImagesWrraper = () => {
             src={"/images/iPhone-2.png"}
           />
         </div>
-        <div className="relative  w-[300px] h-[350px] sm:w-[350px] sm:h-[410px]">
+        <div className="relative w-[200px] h-[240px] sm:w-[350px] sm:h-[410px]">
           <Image
             fill
             className="object-center"
