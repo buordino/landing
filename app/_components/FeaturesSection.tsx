a5268a9f7fd6ad72f7277e46432888a82ef118b8
unknown <mh.naderi1386@gmail.com>
2025-04-23 14:53:57 +0330

    change ui

--- app/_components/FeaturesSection.tsx
+++ app/_components/FeaturesSection.tsx
@@ -1,44 +1,47 @@
 import Image from "next/image";
-import FeatureTexts from "./FeatureTexts"
-import SectionTitle from "./SectionTitle"
+import FeatureTexts from "./FeatureTexts";
+import SectionTitle from "./SectionTitle";
 
 const FeaturesSection = () => {
   return (
     <section id="features">
       <SectionTitle>ویژگی ها</SectionTitle>
-      <div className="py-22 px-16 md:px-42">
-        <div className="flex items-start justify-between gap-6">
-          <div className="w-[30%] pt-14">
-            <FeatureTexts
-              symbol="star"
-              texts={[
-                "لورم ایپسوم متن ساختگی با تولیسادگی ناملورمایپسوم متساختگی با تولیسادگی نام لورم ایپ",
-                "لورم ایپسوم متن ساختگی با تولیسادگی ناملورمایپسوم متساختگی با تولیسادگی نام لورم ایپ",
-                "لورم ایپسوم متن ساختگی با تولیسادگی ناملورمایپسوم متساختگی با تولیسادگی نام لورم ایپ",
-              ]}
-            />
-          </div>
-          <div className="h-[600px] relative w-[25%]">
-            <Image
-              fill
-              className="object-center"
-              alt="mobile-img"
-              src={"/images/Group-41.png"}
-            />
-          </div>
-          <div className="w-[30%] pt-14">
-            <FeatureTexts
-              symbol="number"
-              texts={[
-                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non obcaecati nulla reprehenderit fugiat harum delectus!",
-                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non obcaecati nulla reprehenderit fugiat harum delectus!",
-              ]}
-            />
+      <div className="relative">
+        <div className="absolute w-[100%] bg-gradient-to-b from-custome-orange-1 to-circle-color blur-[125px]   left-0 right-0 top-0 mx-auto rounded-b-[100%]"></div>
+        <div className="relative py-22 px-16 md:px-42">
+          <div className="flex items-start justify-between gap-6">
+            <div className="w-[30%] pt-18">
+              <FeatureTexts
+                symbol="star"
+                texts={[
+                  "لورم ایپسوم متن ساختگی با تولیسادگی ناملورمایپسوم متساختگی با تولیسادگی نام لورم ایپ",
+                  "لورم ایپسوم متن ساختگی با تولیسادگی ناملورمایپسوم متساختگی با تولیسادگی نام لورم ایپ",
+                  "لورم ایپسوم متن ساختگی با تولیسادگی ناملورمایپسوم متساختگی با تولیسادگی نام لورم ایپ",
+                ]}
+              />
+            </div>
+            <div className="h-[600px] relative w-[25%]">
+              <Image
+                fill
+                className="object-center"
+                alt="mobile-img"
+                src={"/images/Group-41.png"}
+              />
+            </div>
+            <div className="w-[30%] pt-18">
+              <FeatureTexts
+                symbol="number"
+                texts={[
+                  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non obcaecati nulla reprehenderit fugiat harum delectus!",
+                  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non obcaecati nulla reprehenderit fugiat harum delectus!",
+                ]}
+              />
+            </div>
           </div>
         </div>
       </div>
     </section>
   );
-}
+};
 
-export default FeaturesSection
+export default FeaturesSection;
