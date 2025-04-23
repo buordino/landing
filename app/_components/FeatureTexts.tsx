8c1ae6b293f5048a6e9c1bf24835bb0a9a64eedb
unknown <mh.naderi1386@gmail.com>
2025-04-23 14:04:26 +0330

    add a reusable component

--- app/_components/FeatureTexts.tsx
+++ app/_components/FeatureTexts.tsx
@@ -1,17 +1,36 @@
+import Image from "next/image";
+
 interface FeatureTextsPropsType {
   texts: string[];
   symbol: "number" | "star";
 }
 const FeatureTexts = ({ texts, symbol }: FeatureTextsPropsType) => {
-
-  return <div className="flex flex-col gap-5">
-    {texts.map((text,index)=><div className="font-semibold text-custome-black flex items-start gap-2" key={index+1}>
-      
-    <p>{text}</p>      
-
-    </div>)}
-
-  </div>;
+  return (
+    <div className="flex flex-col gap-5">
+      {texts.map((text, index) => (
+        <div
+          className="font-semibold text-custome-black flex items-start gap-2"
+          key={index + 1}
+        >
+          {symbol === "number" ? (
+            <span className="font-bold text-custome-orange-2">
+              ({index + 1}
+            </span>
+          ) : (
+            <div className="relative size-4">
+              <Image
+                fill
+                className="object-center"
+                alt="star-1"
+                src={"/images/small-star-2"}
+              />
+            </div>
+          )}
+          <p>{text}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default FeatureTexts;
