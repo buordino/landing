b4e07dab6461b7cfe54dc0cd101a3997c448e992
unknown <mh.naderi1386@gmail.com>
2025-04-25 14:12:22 +0330

    change some props

--- app/_components/FeaturesSection.tsx
+++ app/_components/FeaturesSection.tsx
@@ -32,8 +32,8 @@ const FeaturesSection = () => {
               <FeatureTexts
                 symbol="number"
                 texts={[
-                  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non obcaecati nulla reprehenderit fugiat harum delectus!",
-                  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non obcaecati nulla reprehenderit fugiat harum delectus!",
+                  "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.",
+                  "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.",
                 ]}
               />
             </div>
