d2ffc027d6e6b21aa1d8759b11b22b5b5bfe891c
unknown <mh.naderi1386@gmail.com>
2025-04-27 14:07:44 +0330

    change ui

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,11 +1,15 @@
 import Logo from "./Logo";
 import MobileMenu from "./MobileMenu";
 import Navigation from "./Navigation";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 const Header = () => {
   return (
     <header>
       <div className="flex justify-between md:justify-normal items-center px-12 md:px-28 py-4">
+        <div>
+          <ThemeSwitcher />
+        </div>
         <div className="hidden md:grow md:flex md:items-center md:justify-center">
           <Navigation isShow />
         </div>
