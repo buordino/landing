f2d56128f99a58ce6def6f110291c5025c34a0cc
unknown <mh.naderi1386@gmail.com>
2025-04-27 13:52:10 +0330

    add footer stars

--- app/_components/FooterStar.tsx
+++ app/_components/FooterStar.tsx
@@ -1,15 +1,33 @@
+"use client";
 import Image from "next/image";
-
-const FooterStar = () => {
+import { motion } from "framer-motion";
+interface FooterStarPropsType {
+  left: string;
+  top: string;
+}
+const FooterStar = ({ left, top }: FooterStarPropsType) => {
   return (
-    <span className="size-[40px] absolute left-5 top-3">
+    <motion.span
+      animate={{
+        rotate: [0, 180, 360],
+        x: [0, 10, 0, -10, 0],
+        y: [0, -10, 0, 10, 0],
+      }}
+      transition={{
+        duration: 8,
+        ease: "easeInOut",
+        repeat: Infinity,
+      }}
+      className={`size-[35px] absolute  `}
+      style={{ left: left, top: top }}
+    >
       <Image
-        src={"/images/star-footer.png"}
+        src="/images/star-footer.png"
         alt="star-img"
         fill
         className="object-cover"
       />
-    </span>
+    </motion.span>
   );
 };
 
