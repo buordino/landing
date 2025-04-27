88af46ad58dd3fff7572965b854786c01aa046c2
unknown <mh.naderi1386@gmail.com>
2025-04-28 00:06:14 +0330

    change ui

--- app/_components/DevelopersSection.tsx
+++ app/_components/DevelopersSection.tsx
@@ -3,9 +3,9 @@ import SectionTitle from "./SectionTitle";
 
 const DevelopersSection = () => {
   return (
-    <section id="developers" className="dark:bg-custome-black">
+    <section id="developers" className="dark:bg-custome-black pt-32">
       <SectionTitle>توسعه دهندگان</SectionTitle>
-      <div className="py-22 px-12  2lg:px-42">
+      <div className="py-18 px-12  2lg:px-42">
         <div>
           <DevelopersList />
         </div>
