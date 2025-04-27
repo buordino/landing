3e600a9935a1b3451e71a0f9f161561d5a15c6ee
unknown <mh.naderi1386@gmail.com>
2025-04-27 13:05:44 +0330

    add some apps links

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,21 +1,21 @@
+import FooterAppsLinks from "./FooterAppsLinks";
 import Logo from "./Logo";
-import Navigation from "./Navigation"
+import Navigation from "./Navigation";
 
 const Footer = () => {
   return (
     <footer className="pt-16 pb-20 border-t border-solid border-[#00000033]/90">
       <div className="flex flex-col  items-center justify-center">
-        <Logo/>
+        <Logo />
         <div className="mt-6">
-        <Navigation />
-
+          <Navigation />
         </div>
         <div className="mt-8">
-
+          <FooterAppsLinks />
         </div>
       </div>
     </footer>
   );
-}
+};
 
-export default Footer
+export default Footer;
---
+++ app/_components/FooterAppsLinks.tsx
@@ -0,0 +1,30 @@
+import { TiSocialYoutube } from "react-icons/ti";
+import { FaInstagram } from "react-icons/fa6";
+import Link from "next/link";
+
+const apps = [
+  { href: "/", icon: <FaInstagram size={25} />, color: "#59168b" },
+  { href: "/", icon: <TiSocialYoutube size={25} />, color: "#e7000b" },
+];
+const FooterAppsLinks = () => {
+  return (
+    <ul className="flex items-center gap-3">
+      {apps.map((app, index) => (
+        <li key={index + 1}>
+          <Link
+            className={`transition-all duration-150 ${
+              app.color === "#59168b"
+                ? "hover:text-[#59168b]"
+                : "hover:text-[#e7000b]"
+            } `}
+            href={app.href}
+          >
+            {app.icon}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FooterAppsLinks;
