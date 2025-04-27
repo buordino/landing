1408e05a4b6c27328236b9c4cbaa334bf1316a8b
unknown <mh.naderi1386@gmail.com>
2025-04-27 12:52:12 +0330

    working on footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,8 +1,21 @@
+import Logo from "./Logo";
+import Navigation from "./Navigation"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer className="pt-16 pb-20 border-t border-solid border-[#00000033]/90">
+      <div className="flex flex-col  items-center justify-center">
+        <Logo/>
+        <div className="mt-6">
+        <Navigation />
+
+        </div>
+        <div className="mt-8">
+
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer
