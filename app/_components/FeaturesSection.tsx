eb1c668d4a9a1bf60796756eb842c5c3c4332e5c
unknown <mh.naderi1386@gmail.com>
2025-04-29 21:41:12 +0330

    so far

--- app/_components/FeaturesSection.tsx
+++ app/_components/FeaturesSection.tsx
@@ -7,7 +7,7 @@ const FeaturesSection = () => {
       <SectionTitle>ویژگی ها</SectionTitle>
       <div className="relative">
         <div className="absolute w-[100%] bg-gradient-to-b from-custome-orange-1 to-circle-color blur-[125px]   left-0 right-0 top-0 bottom-[25%] mx-auto rounded-b-[100%]"></div>
-        <div className="relative py-22 px-5 sm:px-12 2lg:px-64">
+        <div className="relative pb-22 pt-18 px-5 sm:px-12 2lg:px-64">
           <FeatureContent />
         </div>
       </div>
