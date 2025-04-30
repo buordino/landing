84397d99fd6913e8005e4bb6da4b1e7d1afcc1ab
unknown <mh.naderi1386@gmail.com>
2025-04-30 13:43:31 +0330

    change scroll button

--- app/_components/ScrollBtn.tsx
+++ app/_components/ScrollBtn.tsx
@@ -41,7 +41,7 @@ const ScrollBtn = () => {
           animate={{ x: 20, opacity: 1 }}
           exit={{ x: -20, opacity: 0 }}
           onClick={scrollToTop}
-          className="w-[52px] h-[52px] z-[1000] shadow-md fixed cursor-pointer left-4 bottom-20 bg-custome-orange-2 flex justify-center items-center rounded-[100%]"
+          className="w-[52px] h-[52px] z-[1000] shadow-md fixed cursor-pointer left-0.5 bottom-14 bg-custome-orange-2 flex justify-center items-center rounded-[100%]"
         >
           <FaChevronUp fill="white" size={22} />
         </motion.button>
