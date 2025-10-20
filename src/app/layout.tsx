5398cc4081ed09c9821a1b8c59b8e79214d0b270
unknown <mh.naderi1386@gmail.com>
2025-10-20 21:41:53 +0330

    refactor(layout): improve layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,30 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+
 export const metadata: Metadata = {
-  title: "بـردیـنـو",
-  description: "با بـردیـنـو، بازی کن، امتیاز بگیر، برنده شو!",
-  icons:{
-    icon:"/images/logo.png"
-  }
+  title: "بـردیـنـو | بازی کن، امتیاز بگیر، برنده شو!",
+  description: "با بـردیـنـو تجربه‌ای متفاوت از سرگرمی و رقابت داشته باش.",
+  icons: {
+    icon: "/images/logo.png",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="fa" dir="rtl">
-      <body className="overflow-x-hidden h-dvh bg-background text-white">
+      <body className="bg-background text-white min-h-screen flex flex-col overflow-x-hidden">
         <Header />
-        <main>{children}</main>
+        <main className="flex-grow">
+          <div className="max-w-[1300px] mx-auto px-4 lg:px-[75px]">
+            {children}
+          </div>
+        </main>
         <Footer />
       </body>
     </html>
