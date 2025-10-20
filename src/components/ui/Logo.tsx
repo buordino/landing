b857ffba47246219a7721d60052d1d359bd8d917
unknown <mh.naderi1386@gmail.com>
2025-10-20 21:43:03 +0330

    reafctor: improve logo component

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <div className="relative w-20 h-20 sm:w-[95px] sm:h-[95px]">
-      <Image src={"/svgs/logo.svg"} fill alt="Logo" />
+      <Image src={"/svgs/logo.svg"} fill alt="Logo" quality={70} />
     </div>
   );
 };
