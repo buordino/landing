922bb90868f75bc435e770dcecb8c27bdb22c2da
unknown <mh.naderi1386@gmail.com>
2025-10-23 21:55:59 +0330

    feat(app-download): add phone image

--- src/components/sections/appDownload/AppDownloadSection.tsx
+++ src/components/sections/appDownload/AppDownloadSection.tsx
@@ -8,8 +8,8 @@ const AppDownloadSection = () => {
     <section>
       <div className="mt-[52px]">
         <SectionTitle>چرا بـردیـنـو؟</SectionTitle>
-        <div className="mt-[86px] flex flex-row items-center justify-center gap-16">
-          <div className="bg-dark-primary rounded-[64px] flex flex-col items-center gap-[52px] py-10 px-14 w-[520px]">
+        <div className="mt-[86px] flex flex-row items-center justify-center gap-12">
+          <div className="bg-dark-primary rounded-[64px] flex flex-col items-center gap-[52px] py-10 px-14 w-[500px]">
             <CustomButton className="w-full h-[70px] text-white bg-[linear-gradient(92.22deg,#5C74A7_2.19%,#7A8FBF_99.45%)] text-2xl">
               دانلود اپلیکیشن
             </CustomButton>
@@ -44,6 +44,14 @@ const AppDownloadSection = () => {
               </div>
             </CustomButton>
           </div>
+          <div className="w-80 h-[450px] relative">
+            <Image
+              src={"/images/Hero-iPhone-Right.png"}
+              alt="iPhone-Image-1"
+              fill
+              className="object-cover z-10"
+            />
+          </div>{" "}
         </div>
       </div>
     </section>
