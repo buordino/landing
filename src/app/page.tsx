faf3f78a695c95cbcd484e018d9331a33dc262ba
unknown <mh.naderi1386@gmail.com>
2025-10-23 17:31:22 +0330

    feat(info): add info section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Guide from "@/components/sections/guide/Guide";
 import Hero from "@/components/sections/hero/Hero";
+import Info from "@/components/sections/info/Info";
 
 export default function Home() {
   return (
     <div className="mt-[122px]">
       <Hero />
       <Guide />
+      <Info />
     </div>
   );
 }
---
+++ src/components/sections/info/Info.tsx
@@ -0,0 +1,19 @@
+import SectionTitle from "@/components/ui/SectionTitle";
+
+const Info = () => {
+  return (
+    <section>
+      <div className="mt-20">
+        <SectionTitle>چرا بـردیـنـو؟</SectionTitle>
+        <div className="mt-13">
+          <p className="font-medium text-lg">
+            بوردینو فقط یک قرعه‌کشی نیست؛ یه دنیای سرگرمیه که هر کاری توش انجام
+            بدی، شانس تازه‌ای برای بردن بهت می‌ده.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Info;
