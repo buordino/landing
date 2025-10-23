1c23fcce9f45d45261e3f17f4027bb1beb0af80a
unknown <mh.naderi1386@gmail.com>
2025-10-23 17:17:48 +0330

    feat(guide): add inner shadow for the guide wrapper

--- src/components/sections/guide/Guide.tsx
+++ src/components/sections/guide/Guide.tsx
@@ -7,7 +7,7 @@ const Guide = () => {
     <section>
       <div className="mt-32">
         <SectionTitle>راهنمای بـردیـنـو</SectionTitle>
-        <div className="mt-14 bg-light-primary rounded-lg border border-solid border-background py-5 px-5">
+        <div className="mt-14 bg-light-primary rounded-lg border border-solid border-background py-5 px-5 shadow-inner">
           <div className="flex flex-row items-start gap-9">
             <div className="basis-lg">
               <GuidesList />
