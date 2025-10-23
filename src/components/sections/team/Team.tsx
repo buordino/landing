28ec980f12244b9ef2d0bc7d42c00f280ade3bac
unknown <mh.naderi1386@gmail.com>
2025-10-23 22:25:36 +0330

    feat(team): add Team section component

---
+++ src/components/sections/team/Team.tsx
@@ -0,0 +1,13 @@
+import SectionTitle from "@/components/ui/SectionTitle"
+
+const Team = () => {
+  return (
+    <section>
+      <div className="mt-14">
+        <SectionTitle>تیم بـردیـنـو</SectionTitle>
+      </div>
+    </section>
+  );
+}
+
+export default Team
