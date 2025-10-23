151612502f0931de3b163dbd9ca3fbcf4d57a7ef
unknown <mh.naderi1386@gmail.com>
2025-10-23 22:33:43 +0330

    feat(team): add team member type and TeamItems component

---
+++ src/components/sections/team/TeamItems.tsx
@@ -0,0 +1,10 @@
+import { TeamItemsPropsType } from "@/types/TeamTypes";
+
+const TeamItems = ({ teamMember }: TeamItemsPropsType) => {
+  const { avatar, field, name } = teamMember;
+  return <li>
+    
+  </li>;
+};
+
+export default TeamItems;
