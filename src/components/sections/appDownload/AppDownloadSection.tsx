73d2da6d78d3b2861aeb9cf39ede35516ac7ae29
unknown <mh.naderi1386@gmail.com>
2025-10-23 21:49:41 +0330

    feat(app-download): add app download section buttons

--- src/components/sections/appDownload/AppDownloadSection.tsx
+++ src/components/sections/appDownload/AppDownloadSection.tsx
@@ -1,12 +1,49 @@
+import CustomButton from "@/components/ui/CustomButton";
+import DownloadIcon from "@/components/ui/icons/DownloadIcon";
 import SectionTitle from "@/components/ui/SectionTitle";
+import Image from "next/image";
 
 const AppDownloadSection = () => {
   return (
     <section>
       <div className="mt-[52px]">
         <SectionTitle>چرا بـردیـنـو؟</SectionTitle>
-        <div className="mt-[86px]">
-            
+        <div className="mt-[86px] flex flex-row items-center justify-center gap-16">
+          <div className="bg-dark-primary rounded-[64px] flex flex-col items-center gap-[52px] py-10 px-20 w-[580px]">
+            <CustomButton className="w-full h-[70px] text-white bg-[linear-gradient(92.22deg,#5C74A7_2.19%,#7A8FBF_99.45%)] text-2xl">
+              دانلود اپلیکیشن
+            </CustomButton>
+            <CustomButton className="w-full h-[70px] bg-white text-lg font-light text-black">
+              <div className="flex flex-row items-center w-full justify-center gap-3.5 pt-2">
+                <Image
+                  src={"/images/Play-Store.png"}
+                  alt="Play-Store"
+                  width={40}
+                  height={40}
+                />
+                <span>دانلود از google play</span>
+              </div>
+            </CustomButton>{" "}
+            <CustomButton className="w-full h-[70px] bg-white text-lg font-light text-black">
+              <div className="flex flex-row items-center w-full justify-center gap-3.5 pt-2">
+                <Image
+                  src={"/images/Miket.png"}
+                  alt="Play-Store"
+                  width={40}
+                  height={40}
+                />
+                <span>دانلود از مایکت</span>
+              </div>
+            </CustomButton>
+            <CustomButton className="w-full h-[70px] bg-white text-lg font-light text-black">
+              <div className="flex flex-row items-center w-full justify-center gap-3.5 pt-2">
+                <div className="w-10 h-10 rounded-full bg-secondary flex items-center justify-center">
+                  <DownloadIcon className="fill-white stroke-white size-6" />
+                </div>
+                <span>دانلود مستقیم</span>
+              </div>
+            </CustomButton>
+          </div>
         </div>
       </div>
     </section>
