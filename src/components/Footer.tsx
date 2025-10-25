f7d36939f9538f1ed52f37aae31099457bdfb36e
unknown <mh.naderi1386@gmail.com>
2025-10-25 21:31:58 +0330

    feat(footer): add left side of the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 import InstagramIcon from "./ui/icons/InstagramIcon";
 import YoutubeIcon from "./ui/icons/YoutubeIcon";
 import ArrowLeft from "./ui/icons/ArrowLeft";
+import SMSIcon from "./ui/icons/SMSIcon";
+import CallIcon from "./ui/icons/CallIcon";
+import LocationIcon from "./ui/icons/LocationIcon";
 
 const Footer = () => {
   return (
-    <footer className="block max-w-[1200px] mx-auto mt-72 px-4 lg:px-[75px] mb-12">
+    <footer className="block max-w-[1250px] mx-auto mt-72 px-4 lg:px-[75px] mb-12">
       <div className="w-full bg-secondary rounded-2xl py-7 px-10 shadow-5xl flex flex-row items-start">
         <div>
           <p className="font-medium text-sm leading-8 w-[340px]">
@@ -28,38 +31,55 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="mr-24">
-          <ul className="flex flex-col gap-5">
+        <div className="mr-24 w-44">
+          <ul className="flex flex-col items-stretch gap-5">
             <li>
-              <div className="flex flex-row items-center gap-0.5">
-                <Link href={"/"} className="font-medium text-base">
-                  درباره برنامه
-                </Link>
+              <Link href={"/"} className="flex flex-row items-center gap-1 ">
+                <span className="font-medium text-base">درباره برنامه</span>
                 <ArrowLeft className="fill-[#AFC3EB] size-4 pt-1" />
+              </Link>
+            </li>{" "}
+            <li>
+              <Link href={"/"} className="flex flex-row items-center gap-0.5">
+                <span className="font-medium text-base">ویژگی ها</span>
+                <ArrowLeft className="fill-[#AFC3EB] size-4 pt-1" />
+              </Link>
+            </li>{" "}
+            <li>
+              <Link href={"/"} className="flex flex-row items-center gap-0.5">
+                <span className="font-medium text-base">توسعه دهندگان</span>
+                <ArrowLeft className="fill-[#AFC3EB] size-4 pt-1" />
+              </Link>
+            </li>{" "}
+            <li>
+              <Link href={"/"} className="flex flex-row items-center gap-0.5">
+                <span className="font-medium text-base">صفحه اصلی</span>
+                <ArrowLeft className="fill-[#AFC3EB] size-4 pt-1" />
+              </Link>
+            </li>{" "}
+          </ul>
+        </div>
+        <div className="mr-24 mt-5">
+          <ul className="flex flex-col gap-4">
+            <li>
+              <div className="flex flex-row items-center gap-2.5">
+                <SMSIcon className={"w-6 h-6 stroke-white fill-none"} />
+                <span className="text-base font-medium">info@pilsun.com</span>
               </div>
             </li>{" "}
             <li>
-              <div className="flex flex-row items-center gap-0.5">
-                <Link href={"/"} className="font-medium text-base">
-                  ویژگی ها{" "}
-                </Link>
-                <ArrowLeft className="fill-[#AFC3EB] size-4 pt-1" />
+              <div className="flex flex-row items-center gap-2.5">
+                <CallIcon className="w-6 h-6 fill-none stroke-white" />
+                <span className="text-base font-medium">021662627</span>
               </div>
             </li>{" "}
             <li>
-              <div className="flex flex-row items-center gap-0.5">
-                <Link href={"/"} className="font-medium text-base">
-                  توسعه دهندگان{" "}
-                </Link>
-                <ArrowLeft className="fill-[#AFC3EB] size-4 pt-1" />
-              </div>
-            </li>{" "}
-            <li>
-              <div className="flex flex-row items-center gap-0.5">
-                <Link href={"/"} className="font-medium text-base">
-                  صفحه اصلی{" "}
-                </Link>
-                <ArrowLeft className="fill-[#AFC3EB] size-4 pt-1" />
+              <div className="flex flex-row items-start gap-2.5">
+                <LocationIcon className="w-10 h-10 stroke-white fill-none pb-4" />
+                <span className="text-base font-medium">
+                  قم ، میدان عباسی میدان گلها بلوار گلها خیابان مرداد دوم غربی
+                  پلاک 4{" "}
+                </span>
               </div>
             </li>
           </ul>
--- src/components/ui/icons/SMSIcon.tsx
+++ src/components/ui/icons/SMSIcon.tsx
@@ -1,16 +1,14 @@
+import { IconPropsType } from "@/types/IconTypes";
 
-const SMSIcon = () => {
+const SMSIcon = ({ className }: IconPropsType) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
-      fill="none"
+      className={className}
     >
       <path
         d="M17 20.5H7C4 20.5 2 19 2 15.5V8.5C2 5 4 3.5 7 3.5H17C20 3.5 22 5 22 8.5V15.5C22 19 20 20.5 17 20.5Z"
-        stroke="#F8F1E9"
         strokeWidth="1.5"
         strokeMiterlimit="10"
         strokeLinecap="round"
@@ -26,6 +24,6 @@ const SMSIcon = () => {
       />
     </svg>
   );
-}
+};
 
-export default SMSIcon
+export default SMSIcon;
