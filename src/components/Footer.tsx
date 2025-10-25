390d1f0d398aa98b27d0e0c36114a334d0eb22fe
unknown <mh.naderi1386@gmail.com>
2025-10-25 20:55:29 +0330

    feat(footer): add links for sections

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
 import InstagramIcon from "./ui/icons/InstagramIcon";
 import YoutubeIcon from "./ui/icons/YoutubeIcon";
+import ArrowLeft from "./ui/icons/ArrowLeft";
 
 const Footer = () => {
   return (
     <footer className="block max-w-[1200px] mx-auto mt-72 px-4 lg:px-[75px] mb-12">
-      <div className="w-full bg-secondary rounded-2xl py-7 px-10 block shadow-5xl">
+      <div className="w-full bg-secondary rounded-2xl py-7 px-10 shadow-5xl flex flex-row items-start">
         <div>
-          <p className="font-medium text-sm leading-8">
+          <p className="font-medium text-sm leading-8 w-[340px]">
             لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
             استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در
             ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و
@@ -17,10 +19,50 @@ const Footer = () => {
               ما را در شبکه های اجتماعی دنبال کنید
             </p>
             <div className="flex flex-row items-center gap-3">
-              <InstagramIcon className="w-7 h-7 fill-white" />
-              <YoutubeIcon className="w-6 h-6 fill-white" />
+              <Link href={"/"}>
+                <InstagramIcon className="w-7 h-7 fill-white" />
+              </Link>
+              <Link href={"/"}>
+                <YoutubeIcon className="w-6 h-6 fill-white" />
+              </Link>
             </div>
           </div>
+        </div>
+        <div className="mr-24">
+          <ul className="flex flex-col gap-5">
+            <li>
+              <div className="flex flex-row items-center gap-0.5">
+                <Link href={"/"} className="font-medium text-base">
+                  درباره برنامه
+                </Link>
+                <ArrowLeft className="fill-[#AFC3EB] size-4 pt-1" />
+              </div>
+            </li>{" "}
+            <li>
+              <div className="flex flex-row items-center gap-0.5">
+                <Link href={"/"} className="font-medium text-base">
+                  ویژگی ها{" "}
+                </Link>
+                <ArrowLeft className="fill-[#AFC3EB] size-4 pt-1" />
+              </div>
+            </li>{" "}
+            <li>
+              <div className="flex flex-row items-center gap-0.5">
+                <Link href={"/"} className="font-medium text-base">
+                  توسعه دهندگان{" "}
+                </Link>
+                <ArrowLeft className="fill-[#AFC3EB] size-4 pt-1" />
+              </div>
+            </li>{" "}
+            <li>
+              <div className="flex flex-row items-center gap-0.5">
+                <Link href={"/"} className="font-medium text-base">
+                  صفحه اصلی{" "}
+                </Link>
+                <ArrowLeft className="fill-[#AFC3EB] size-4 pt-1" />
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </footer>
