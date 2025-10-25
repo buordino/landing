e049c92dcb4ddaaa882aa33800f84721fac2a901
unknown <mh.naderi1386@gmail.com>
2025-10-25 20:13:41 +0330

    feat(footer): add footer component and improve some ui points

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
-
 const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
-}
+    <footer className="block max-w-[1200px] mx-auto mt-72 px-4 lg:px-[75px]">
+      <div className="w-full bg-secondary rounded-2xl py-7 px-10 block shadow-5xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
