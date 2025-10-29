cdd28f577596aedb0b031cb27f120f0f754d339a
unknown <mh.naderi1386@gmail.com>
2025-10-29 15:29:03 +0330

    feat(header): make header circles responsive

---
+++ src/components/ui/icons/HeaderCircleLightSmall.tsx
@@ -0,0 +1,35 @@
+const HeaderCircleLightSmall = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="161"
+      height="198"
+      viewBox="0 0 161 198"
+      fill="none"
+    >
+      <ellipse
+        cx="302.321"
+        cy="254.324"
+        rx="302.321"
+        ry="254.324"
+        transform="matrix(0.585081 -0.810975 0.782592 0.622536 -482 -7.65088)"
+        fill="url(#paint0_linear_1_5060)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_1_5060"
+          x1="604.641"
+          y1="254.324"
+          x2="0"
+          y2="254.324"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#A3BFFA" />
+          <stop offset="1" stopColor="#5C74A7" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default HeaderCircleLightSmall;
