9937e460f9f2c31317d8fc51d267166a9bdc5cc3
unknown <mh.naderi1386@gmail.com>
2025-10-29 19:02:03 +0330

    refactor(guide): improve responsive of Guide section

--- src/components/sections/guide/GuidesList.tsx
+++ src/components/sections/guide/GuidesList.tsx
@@ -10,7 +10,7 @@ const guides: GuideType[] = [
     title: "گردونه و کوییز",
     icon: <GamePadIcon className="stroke-white fill-none w-8 h-8" />,
     description: (
-      <p className="text-white font-medium text-base md:text-lg leading-8">
+      <p className="text-white font-medium text-sm md:text-lg leading-8">
         این بخش از اپ تو میتونی امتیاز بگیری و باهاش توی{" "}
         <span className="text-background">قرعه کشی ها</span> شرکت کنی <br /> با{" "}
         <span className="text-background">بازی کردن</span> امتیاز میگیری و اون
@@ -22,7 +22,7 @@ const guides: GuideType[] = [
     title: "کیف پول",
     icon: <WalletIcon className="fill-none stroke-white w-8 h-8" />,
     description: (
-      <p className="text-white font-medium text-base md:text-lg leading-8">
+      <p className="text-white font-medium text-sm md:text-lg leading-8">
         اینجا تو <span className="text-background">موجودیتو</span> میبینی و
         میتونی هم <span className="text-background">واریز</span> کنی و هم واریزی
         هاتو ببینی <br /> با واریز کردن میتونی{" "}
@@ -35,7 +35,7 @@ const guides: GuideType[] = [
     title: "دعوت دوستان",
     icon: <UsersIcon className="stroke-white fill-none w-8 h-8" />,
     description: (
-      <p className="text-white font-medium text-base md:text-lg leading-8">
+      <p className="text-white font-medium text-sm md:text-lg leading-8">
         میتونی <span className="text-background">دوستاتو</span> دعوت کنی و با
         دعوت کردن هم میتونی جایزه بگیری <br /> دعوتی که انجام بدی و{" "}
         <span className="text-background">اولین واریزشو</span> انجام بده{" "}
@@ -48,7 +48,7 @@ const guides: GuideType[] = [
     title: "جوایز",
     icon: <GiftIcon className="w-8 h-8 fill-none stroke-white" />,
     description: (
-      <p className="text-white font-medium text-base md:text-lg leading-8">
+      <p className="text-white font-medium text-sm md:text-lg leading-8">
         اینجا <span className="text-background">جایزه</span> هارو میتونی ببینی و
         هرکدوم که خواستی توی <span className="text-background">قرعه کشی</span>{" "}
         شرکت کنی <br /> برای شرکت تو قرعه کشی میتونی کوپن رو{" "}
