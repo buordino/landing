364a1ff761840256205aeabf046f803bb12ca4c1
unknown <mh.naderi1386@gmail.com>
2025-11-01 17:42:38 +0330

    refactor: improve ui of SectionTitle component

--- src/components/ui/SectionTitle.tsx
+++ src/components/ui/SectionTitle.tsx
@@ -5,11 +5,15 @@ type SectionTitlePropsType = {
 };
 const SectionTitle = ({ children }: SectionTitlePropsType) => {
   return (
-    <div className="flex mx-auto w-52 flex-col gap-2.5">
-      <h3 className="text-white text-2xl md:text-[28px] font-medium md:font-bold">{children}</h3>
-      <div className="flex flex-col gap-2">
-        <div className="bg-primary w-12 h-1.5 rounded-full"></div>
-        <div className="bg-primary w-22 h-1.5 rounded-full"></div>
+    <div className="w-full flex flex-row justify-center">
+      <div className="flex mx-auto w-auto flex-col gap-2.5">
+        <h3 className="text-white text-2xl md:text-[28px] font-medium md:font-bold">
+          {children}
+        </h3>
+        <div className="flex flex-col gap-2">
+          <div className="bg-primary w-12 h-1.5 rounded-full"></div>
+          <div className="bg-primary w-22 h-1.5 rounded-full"></div>
+        </div>
       </div>
     </div>
   );
